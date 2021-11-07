f587c90ead96075905337e690f3cbd18d779a489
Rio Andrianto <r.andrianto@gmail.com>
2021-11-07 12:41:11 +0700

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <title>Progress</title>
-</head>
-<body>
-<figure><embed src="https://wakatime.com/share/@531bc5bc-45d5-433d-9eb2-189f41821943/17096574-00e7-4db1-97ae-6bf2639b9943.svg"></embed></figure>
-</body>
-</html>
